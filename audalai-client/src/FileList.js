ead84d4e2361f7b1ce6a85dc2ab246faa68d3d99
Andrew Schwartz <ozydingo@gmail.com>
2019-03-16 16:01:59 -0400

    Add basic file row selection mechanism

--- audalai-client/src/FileList.js
+++ audalai-client/src/FileList.js
@@ -9,15 +9,53 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 const styles = {
+  open: {
+    color: 'blue',
+  },
+  closed: {
+  },
+  hidden: {
+    color: 'grey',
+  },
+  tableCell: {
+    color: 'inherit',
+  }
 };
 
 class FileList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openFile: null,
+    }
+  }
+
+  clickRow(id) {
+    if (this.state.openFile === id) {
+      this.setState({openFile: null});
+    } else {
+      this.setState({openFile: id});
+    }
+  }
+
   fileRow(file) {
+    const { classes } = this.props;
+
+    let className;
+    if (this.state.openFile) {
+      className = (file.id === this.state.openFile) ? classes.open : classes.hidden;
+    } else {
+      className = classes.closed;
+    }
     return (
-      <TableRow key={file.id}>
-        <TableCell component='th' scope='row'>{ file.name }</TableCell>
-        <TableCell>{ file.type }</TableCell>
-        <TableCell>{ file.created }</TableCell>
+      <TableRow
+          key={file.id}
+          className={className}
+          onClick={() => this.clickRow(file.id)}
+      >
+        <TableCell className = {classes.tableCell}  component='th' scope='row'>{ file.name }</TableCell>
+        <TableCell className = {classes.tableCell} >{ file.type }</TableCell>
+        <TableCell className = {classes.tableCell} >{ file.created }</TableCell>
       </TableRow>
     );
   }
