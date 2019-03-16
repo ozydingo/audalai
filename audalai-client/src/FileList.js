839cc952456e2ce3cfeac4a537e0cea9117fcb88
Andrew Schwartz <ozydingo@gmail.com>
2019-03-16 15:32:01 -0400

    Use table for file list

--- audalai-client/src/FileList.js
+++ audalai-client/src/FileList.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const styles = {
+};
+
 class FileList extends Component {
   fileRow(file) {
     return (
-      <div key={file.id}>{ file.name }</div>
+      <TableRow key={file.id}>
+        <TableCell component='th' scope='row'>{ file.name }</TableCell>
+        <TableCell>{ file.type }</TableCell>
+        <TableCell>{ file.created }</TableCell>
+      </TableRow>
     );
   }
 
@@ -12,12 +27,22 @@ class FileList extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        { this.fileTable(this.props.files) }
-      </div>
+      <Table>
+        <TableHead>
+          <TableRow className={classes.tableHead} >
+            <TableCell className={classes.columnName}>Name</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Created</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { this.fileTable(this.props.files) }
+        </TableBody>
+      </Table>
     );
   }
 }
 
-export default FileList;
+export default withStyles(styles)(FileList);
