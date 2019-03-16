ae9d203d66cca8b539915115b5b236b99f8c22a6
Andrew Schwartz <ozydingo@gmail.com>
2019-03-16 19:54:46 -0400

    Add sweet open/closed animations

--- audalai-client/src/FileList.js
+++ audalai-client/src/FileList.js
@@ -4,53 +4,99 @@ import { withStyles } from '@material-ui/core/styles';
 
 import Paper from '@material-ui/core/Paper';
 
+const animationTiming = 'cubic-bezier(1,0,0,1)';
+const animationDuration = '0.5s';
 const styles = {
+  fileListContainer: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'stretch',
+  },
   fileEntry: {
-    height: '3em',
+    padding: '7px',
+    height: '2em',
   },
   open: {
+    zIndex: '2',
+    flexGrow: '1',
     animationName: 'OpenFile',
-    animationTimingFunction: 'cubic-bezier(0.1,0.7,0.7,0.98)',
-    animationDuration: '0.5s',
-    animationFillMode: 'forwards',
+    animationTimingFunction: animationTiming,
+    animationDuration: animationDuration,
   },
   '@keyframes OpenFile': {
+    from: {
+      flexGrow: '0',
+    },
     to: {
-      height: '10em',
+      flexGrow: '1',
     }
   },
   closing: {
     animationName: 'ClosingFile',
-    animationTimingFunction: 'cubic-bezier(0.1,0.7,0.7,0.98)',
-    animationDuration: '0.5s',
-    animationFillMode: 'forwards',
+    animationTimingFunction: animationTiming,
+    animationDuration: animationDuration,
   },
   '@keyframes ClosingFile': {
     from: {
-      height: '10em',
+      flexGrow: '1',
+      zIndex: '2',
     },
     to: {
-      height: '3em',
+      flexGrow: '0',
+      zIndex: '1',
     }
   },
   closed: {
+    flexGrow: '0',
   },
   hidden: {
+    opacity: '0',
+    padding: '0px',
+    height: '0px',
+    padding: '0px',
+    position: 'relative',
     animationName: 'HiddenFile',
-    animationTimingFunction: 'cubic-bezier(0.1,0.7,0.7,0.98)',
-    animationDuration: '0.5s',
-    animationFillMode: 'forwards',
+    animationTimingFunction: animationTiming,
+    animationDuration: animationDuration,
   },
   '@keyframes HiddenFile': {
     from: {
+      left: '0px',
+      width: '100%',
       opacity: '1',
-      height: '10em',
+      padding: '7px',
+      height: '2em',
     },
     to: {
-      opacity: '0.1',
-      height: '3em',
-      display: 'none',
+      left: '-200px',
+      width: '300%',
+      opacity: '0',
+      height: '0px',
+      padding: '0px',
     }
+  },
+  unhiding: {
+    animationName: 'UnhidingFile',
+    animationTimingFunction: animationTiming,
+    animationDuration: animationDuration,
+  },
+  '@keyframes UnhidingFile': {
+    from: {
+      left: '-200px',
+      width: '300%',
+      opacity: '0',
+      height: '0px',
+      padding: '0px',
+    },
+    to: {
+      left: '0px',
+      width: '100%',
+      opacity: '1',
+      padding: '7px',
+      height: '2em',
+    },
   },
 };
 
@@ -63,40 +109,45 @@ class FileList extends Component {
     }
   }
 
-  clickRow(id) {
-    if (this.state.openFile === id) {
-      this.setState({
-        openFile: null,
-        closingFile: id,
-      });
-    } else {
-      this.setState({openFile: id});
-    }
+  closeFile(file) {
+    this.setState({
+      openFile: null,
+      closingFile: file.id,
+    });
   }
 
-  fileClasses(file) {
-    const { classes } = this.props;
-    const isOpen = file.id === this.state.openFile;
-    let fileClasses = [];
-    if (this.state.openFile) {
-      fileClasses.push(isOpen ? classes.open : classes.hidden);
-    } else {
-      fileClasses.push(classes.closed);
-    }
-    if (!isOpen && file.id === this.state.closingFile) {
-      fileClasses.push(classes.closing);
-    }
-    fileClasses.push(classes.fileEntry);
-    return fileClasses;
+  openFile(file) {
+    this.setState({openFile: file.id});
   }
 
   fileEntry(file) {
-    const fileClasses = this.fileClasses(file);
+    const { classes } = this.props;
+    const isOpen = file.id === this.state.openFile;
+    let fileClasses = [];
+    let clickHandler = () => null;
+    if (this.state.openFile) {
+      if (isOpen) {
+        fileClasses.push(classes.open);
+        clickHandler = () => this.closeFile(file);
+      } else {
+        fileClasses.push(classes.hidden);
+      }
+    } else {
+      fileClasses.push(classes.closed);
+      clickHandler = () => this.openFile(file);
+      if (!isOpen && file.id === this.state.closingFile) {
+        fileClasses.push(classes.closing);
+      } else {
+        fileClasses.push(classes.unhiding);
+      }
+    }
+    fileClasses.push(classes.fileEntry);
+
     return (
       <Paper
           key={file.id}
           className={fileClasses.join(' ')}
-          onClick={() => this.clickRow(file.id)}
+          onClick={clickHandler}
           square={true}
       >
         <div>{ file.name }</div>
@@ -104,15 +155,11 @@ class FileList extends Component {
     );
   }
 
-  fileTable(files) {
-    return files.map(file => this.fileEntry(file));
-  }
-
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        { this.fileTable(this.props.files) }
+      <div className = {classes.fileListContainer} >
+        { this.props.files.map(file => this.fileEntry(file)) }
       </div>
     );
   }
