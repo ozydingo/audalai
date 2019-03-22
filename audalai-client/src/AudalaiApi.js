d3b2f3a925375c463349fdc8162855dcfe9a23b0
Andrew Schwartz <ozydingo@gmail.com>
2019-03-22 03:15:23 -0400

    feat(client) Fetch getFiles data from cloud function

--- audalai-client/src/AudalaiApi.js
+++ audalai-client/src/AudalaiApi.js
@@ -1,12 +1,15 @@
+const axios = require('axios');
+
 class AudalaiApi {
   getFiles() {
-    return Promise.resolve({
-      data: [
-        {index: 1, id: 1, name: 'lorem.mp3'},
-        {index: 2, id: 2, name: 'ipsum.ogg'},
-        {index: 3, id: 5, name: 'dolor.wav'},
-      ]
-    });
+    return axios.get("https://us-east1-audalai.cloudfunctions.net/guest-list-files");
+    // return Promise.resolve({
+    //   data: [
+    //     {index: 1, id: 1, name: 'lorem.mp3'},
+    //     {index: 2, id: 2, name: 'ipsum.ogg'},
+    //     {index: 3, id: 5, name: 'dolor.wav'},
+    //   ]
+    // });
   }
 }
 
