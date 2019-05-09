24670edffb07e06fc25c40fc895b223a0f573288
Andrew Schwartz <ozydingo@gmail.com>
2019-05-09 11:19:29 -0400

    feat(dev) Add audalai.test to config.hosts to support puma-dev

--- server/config/application.rb
+++ server/config/application.rb
@@ -15,5 +15,9 @@ module Server
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    if Rails.env == "development"
+      config.hosts << "audalai.test"
+    end
   end
 end
