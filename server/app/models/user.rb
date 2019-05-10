2bc2fbb3f21480173114bbfec431c488d56b413c
Andrew Schwartz <ozydingo@gmail.com>
2019-05-10 08:37:30 -0400

    feat(server/user) Create audios with write access by default

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_and_belongs_to_many :audios, join_table: "audio_read_accesses"
 
   validates :email, presence: true, uniqueness: true
+
+  # Override audios association method to ensure that
+  # creationg comes with write permission by default.
+  def audios
+    super.create_with(writers: [self])
+  end
 end
--- server/test/models/user_test.rb
+++ server/test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "audios are created with write access" do
+    audio = @user.audios.create!(cloud_file: CloudFile.new)
+    re_audio = Audio.find(audio.id)
+
+    assert_includes(re_audio.writers, @user)
+  end
 end
