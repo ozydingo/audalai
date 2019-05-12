fcfc7c8e11d48474eb1693b7c55c7ba92e2832fa
Andrew Schwartz <ozydingo@gmail.com>
2019-05-12 08:43:04 -0400

    feat(server/db) Add db:seed data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "Andrew Schwartz",
+  nickname: "Andrew",
+  email: "schwartz.andrew.h@gmail.com",
+  password: SecureRandom.alphanumeric(12),
+)
+
+guest = User.create!(
+  name: "Guest User",
+  nickname: "Guest",
+  email: "guest@audalai.com",
+  password: SecureRandom.alphanumeric(12),
+)
+
+guest.audios.create!(
+  name: "foo.wav",
+  cloud_file: CloudFile.new,
+)
+
+guest.audios.create!(
+  name: "bar.mp3",
+  cloud_file: CloudFile.new,
+)
