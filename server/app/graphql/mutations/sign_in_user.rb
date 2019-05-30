e54318432ba5ca1375275a96fe706477fc5ce6f5
Andrew Schwartz <ozydingo@gmail.com>
2019-05-30 05:06:27 -0400

    feat(server) Add sign_in_user mutation

---
+++ server/app/graphql/mutations/sign_in_user.rb
@@ -0,0 +1,23 @@
+module Mutations
+  class SignInUser < BaseMutation
+    null true
+
+    argument :email, Types::AuthProviderEmailInput, required: false
+
+    field :token, String, null: true
+    field :user, Types::UserType, null:true
+
+    def resolve(email: nil)
+      email.present? or return nil
+      user = User.find_by(email: email[:email]) or return nil
+      user.authenticate(email[:password]) or return nil
+
+      token = Authenticator.new.generate_token(email[:email], email[:password])
+
+      return {
+        user: user,
+        token: token,
+      }
+    end
+  end
+end
