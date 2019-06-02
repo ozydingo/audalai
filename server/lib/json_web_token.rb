b7ad9211291279e6908694ef4a67291ed4d93e44
Andrew Schwartz <ozydingo@gmail.com>
2019-06-02 02:16:28 -0400

    feat(server/auth) Allow decoding of expired tokens

--- server/lib/json_web_token.rb
+++ server/lib/json_web_token.rb
@@ -1,15 +1,14 @@
 module JsonWebToken
   class << self
-    def encode(payload, exp = 24.hours.from_now)
+    def encode(payload, exp: 24.hours.from_now)
       payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
-    def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-      HashWithIndifferentAccess.new body
-    rescue
-      nil
+    # If validate, raises JWT::ExpiredSignature if expired
+    def decode(token, validate: true)
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base, validate)[0]
+      HashWithIndifferentAccess.new(body)
     end
   end
 end
