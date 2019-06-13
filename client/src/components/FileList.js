01b189dba2edc5e03495162471e0da45d084187b
Andrew Schwartz <ozydingo@gmail.com>
2019-06-13 03:00:54 -0400

    refactor(client/FileList) Rewrite FileList components with hooks

--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 
@@ -13,46 +13,35 @@ const styles = {
   },
 };
 
-class FileList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeFile: null,
-      activeFileOpen: false,
-    }
+function FileList(props) {
+  const [activeFile, setActiveFile] = useState(null);
+  const [activeFileOpen, setActiveFileOpen] = useState(false);
+
+  function closeFile(file) {
+    setActiveFile(file);
+    setActiveFileOpen(false);
   }
 
-  closeFile(file) {
-    this.setState({
-      activeFile: file,
-      activeFileOpen: false,
-    });
+  function openFile(file) {
+    setActiveFile(file);
+    setActiveFileOpen(true);
   }
 
-  openFile(file) {
-    this.setState({
-      activeFile: file,
-      activeFileOpen: true,
-    });
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className = {classes.fileListContainer} >
-        { this.props.files.map(file => {
-          return (
-            <FileEntry
-                file={file}
-                activeFile={this.state.activeFile}
-                activeFileOpen={this.state.activeFileOpen}
-                openFile={this.openFile.bind(this)}
-                closeFile={this.closeFile.bind(this)} />
-          )
-        })}
-      </div>
-    );
-  }
+  const { classes } = props;
+  return (
+    <div className = {classes.fileListContainer} >
+      { props.files.map(file => {
+        return (
+          <FileEntry
+              file={file}
+              activeFile={activeFile}
+              activeFileOpen={activeFileOpen}
+              openFile={openFile}
+              closeFile={closeFile} />
+        )
+      })}
+    </div>
+  );
 }
 
 FileList.propTypes = {
