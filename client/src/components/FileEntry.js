fd72f87c5ffafb160c85e413121c2356301d1fbc
Andrew Schwartz <ozydingo@gmail.com>
2019-06-13 02:53:57 -0400

    refactor(client/FileList) Pull FileItem out into its own component file

---
+++ client/src/components/FileEntry.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+import Paper from '@material-ui/core/Paper';
+import FileContents from './FileContents'
+
+import { fileAnimationStyles } from '../styles/fileListAnimation'
+
+const rootStyles = {
+  fileEntry: {
+    position: 'relative',
+    padding: '7px',
+    height: '2em',
+    overflow: 'scroll',
+  },
+};
+// TODO: Allow assign order to be reversed; 'height' gets overwritten
+const styles = Object.assign({}, rootStyles, fileAnimationStyles);
+
+function FileEntry(props) {
+  const { classes } = props;
+  let fileClasses = [];
+  let clickHandler = () => null;
+
+  if (props.activeFile) {
+    if (props.activeFileOpen) {
+      if (props.file.id === props.activeFile.id) {
+        fileClasses.push(classes.open);
+        clickHandler = () => props.closeFile(props.file);
+      } else if (props.file.index < props.activeFile.index) {
+        fileClasses.push(classes.hiddenAbove, classes.hidden);
+      } else if (props.file.index > props.activeFile.index) {
+        fileClasses.push(classes.hiddenBelow, classes.hidden);
+      }
+    } else {
+      if (props.file.id === props.activeFile.id) {
+        fileClasses.push(classes.closing);
+      } else if (props.file.index < props.activeFile.index) {
+        fileClasses.push(classes.unhidingAbove);
+      } else if (props.file.index > props.activeFile.index) {
+        fileClasses.push(classes.unhidingBelow);
+      }
+    }
+  }
+
+  if (!props.activeFile || !props.activeFileOpen) {
+    fileClasses.push(classes.closed);
+    clickHandler = () => props.openFile(props.file);
+  }
+
+  fileClasses.push(classes.fileEntry);
+
+  const isOpen = props.activeFile &&
+    props.activeFileOpen &&
+    props.file.id === props.activeFile.id;
+
+  return (
+    <Paper
+        key={props.file.id}
+        className={fileClasses.join(' ')}
+        onClick={clickHandler}
+        square={true}
+    >
+      <FileContents file={props.file} open={isOpen} />
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(FileEntry);
--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -2,76 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 
-import Paper from '@material-ui/core/Paper';
+import FileEntry from './FileEntry'
 
-import FileContents from './FileContents'
-import { fileAnimationStyles } from '../styles/fileListAnimation'
-
-const rootStyles = {
+const styles = {
   fileListContainer: {
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'stretch',
   },
-  fileEntry: {
-    position: 'relative',
-    padding: '7px',
-    height: '2em',
-    overflow: 'scroll',
-  },
 };
-// TODO: Allow assign order to be reversed; 'height' gets overwritten
-const styles = Object.assign({}, rootStyles, fileAnimationStyles);
-
-const FileEntry = withStyles(styles)(function(props) {
-  const { classes } = props;
-  let fileClasses = [];
-  let clickHandler = () => null;
-
-  if (props.activeFile) {
-    if (props.activeFileOpen) {
-      if (props.file.id === props.activeFile.id) {
-        fileClasses.push(classes.open);
-        clickHandler = () => props.closeFile(props.file);
-      } else if (props.file.index < props.activeFile.index) {
-        fileClasses.push(classes.hiddenAbove, classes.hidden);
-      } else if (props.file.index > props.activeFile.index) {
-        fileClasses.push(classes.hiddenBelow, classes.hidden);
-      }
-    } else {
-      if (props.file.id === props.activeFile.id) {
-        fileClasses.push(classes.closing);
-      } else if (props.file.index < props.activeFile.index) {
-        fileClasses.push(classes.unhidingAbove);
-      } else if (props.file.index > props.activeFile.index) {
-        fileClasses.push(classes.unhidingBelow);
-      }
-    }
-  }
-
-  if (!props.activeFile || !props.activeFileOpen) {
-    fileClasses.push(classes.closed);
-    clickHandler = () => props.openFile(props.file);
-  }
-
-  fileClasses.push(classes.fileEntry);
-
-  const isOpen = props.activeFile &&
-    props.activeFileOpen &&
-    props.file.id === props.activeFile.id;
-
-  return (
-    <Paper
-        key={props.file.id}
-        className={fileClasses.join(' ')}
-        onClick={clickHandler}
-        square={true}
-    >
-      <FileContents file={props.file} open={isOpen} />
-    </Paper>
-  );
-});
 
 class FileList extends Component {
   constructor(props) {
