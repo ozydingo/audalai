faa158052489ebe47687e8c88a3e476fcc74943a
Andrew Schwartz <ozydingo@gmail.com>
2019-06-15 05:16:28 -0400

    debt(server) Remove rails routes for users and sessions, and remove references in home

--- server/app/controllers/api_tokens_controller.rb
+++
@@ -1,2 +0,0 @@
-class ApiTokensController < ApplicationController
-end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -8,11 +8,4 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   post "/graphql", to: "graphql#execute"
-
-  resources :users
-  resources :sessions, only: :create
-
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
