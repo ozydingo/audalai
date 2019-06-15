515b4660ba2bc91a8d37539bec51aba2ab4dc187
Andrew Schwartz <ozydingo@gmail.com>
2019-06-15 19:47:31 -0400

    fix(JsonWebToken) Use Rails.application.secret_key_base instead of Rails.application.secrets.secret_key_base

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,12 +2,12 @@ module JsonWebToken
   class << self
     def encode(payload, exp: 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.secret_key_base)
     end
 
     # If validate, raises JWT::ExpiredSignature if expired
     def decode(token, validate: true)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base, validate)[0]
+      body = JWT.decode(token, Rails.application.secret_key_base, validate)[0]
       HashWithIndifferentAccess.new(body)
     end
   end
