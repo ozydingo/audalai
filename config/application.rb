991a2d8110132f86e376def65e4595495cd0b5e7
Andrew Schwartz <ozydingo@gmail.com>
2019-06-16 18:48:21 -0400

    fix(server) Set config.assets.enabled = false

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Audalai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.enabled = false
 
     config.autoload_paths << Rails.root.join('lib')
 
